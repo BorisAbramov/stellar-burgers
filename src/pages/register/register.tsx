f7714998a5ef3cf07fdd119d669b4696ea93047d
BorisAbramov <it.school136@gmail.com>
2026-04-19 14:47:49 +0300

    добавление авторизации и регистрации

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,23 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { register } from '../../services/slices/user';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const registerError = useSelector((state) => state.user.registerError);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(register({ name: userName, email, password }));
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={registerError?.message || ''}
       email={email}
       userName={userName}
       password={password}
